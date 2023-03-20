1c1a8e1e4ac64a2527c5ac001153b27d284d0320
dexterdavenport <dexter.davenport1@gmail.com>
2023-03-19 20:35:37 -0700

    Setup/appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 
-function App() {
+import Navbar from './components/Navbar';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#264653",
+      light: "#264653",
+      dark: "#264653",
+    }
+  }
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+
+      <Typography>
+        Hello world
+      </Typography>
+    </ThemeProvider>
   );
 }
-
-export default App;
